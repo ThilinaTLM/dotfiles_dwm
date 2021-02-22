be0fbcc2f487c99885bd3a7d8ff3c1e2bfa2aba5
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-02-22 15:05:17 +0530

    update dblocks

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -1,9 +1,9 @@
 # Source directory of autostart script
 AUTOSTART_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# ~/.dwm/dwmblocks &
-picom &
 urxvtd &
 kdeconnect-cli -l &
 feh $AUTOSTART_DIR/wallpaper --bg-fill &
+~/.dwm/dwmblocks &
+picom &
 
