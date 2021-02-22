b7d279f771d16e206c4362ddbc158a19f2aae2da
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-02-22 12:46:39 +0530

    inialize dwm

---
+++ README.md
@@ -0,0 +1,12 @@
+# My personal DWM build
+Last update: 2020-02-22
+
+# Tools
+feh - to set wallpaper
+picom - compositor
+
+# Patches
+autostart
+
+
+
---
+++ autostart.sh
@@ -0,0 +1,8 @@
+
+#~/.dwm/dwmstatus 2>&1 >/dev/null &
+~/.dwm/dwmblocks &
+picom &
+urxvtd &
+kdeconnect-cli -l &
+feh ./bernardo-lorena-ponte-nUVBOiu5gTA-unsplash.jpg --bg-fill &
+
